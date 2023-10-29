16ea69166e8e07f6b7721807a8d946a716ca62f8
Daniel Guzmán <danieljk001@yahoo.com>
2023-10-28 23:24:17 -0500

    Cambios al login

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -11,33 +11,26 @@ const login = (request, response) => {
     try {
         const {usuario, password} = request.body;
         conexion.query('select * from usuarios where usuario = ?',[usuario], function (err, result, fields) {
-            if(result){
-                if(bcrypt.compareSync(password,result[0].password)){
-                    const data = {
-                        usuario: result[0].usuario
-                    };
-                    const payload = {
-                        check:true
-                    };
-                    const token = jwt.sign({
-                        payload,
-                        data: data
-                    }, config.secret_key, { expiresIn: '1h' });
-                    response.status(200).json({
-                        status: 200,
-                        message: "logged in session successful",
-                        token: token
-                    });
-                }else{
-                    response.status(400).json({
-                        status: 400,
-                        message: "No autorizado, password erronea"
-                    });
-                }
+            if(bcrypt.compareSync(password,result[0].password)){
+                const data = {
+                    usuario: result[0].usuario
+                };
+                const payload = {
+                    check:true
+                };
+                const token = jwt.sign({
+                    payload,
+                    data: data
+                }, config.secret_key, { expiresIn: '1h' });
+                response.status(200).json({
+                    status: 200,
+                    message: "logged in session successful",
+                    token: token
+                });
             }else{
-                response.status(401).json({
-                    status: 401,
-                    message: "No autorizado, email no registrado."
+                response.status(400).json({
+                    status: 400,
+                    message: "No autorizado, password erronea"
                 });
             }
         });
