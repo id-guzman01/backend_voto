715d5890361626b57f1c2bda02f7d364a8514c85
Daniel Guzmán <danieljk001@yahoo.com>
2023-10-28 19:11:34 -0500

    conexion

--- src/database/conexion.database.js
+++ src/database/conexion.database.js
@@ -2,11 +2,11 @@ import mysql from "promise-mysql";
 import config from "../config"
 
 const connection = mysql.createConnection({
-    host: config.host,
-    database: config.database,
-    user: config.user,
-    password: config.password,
-    port: 3306
+    host: 'localhost',
+    database: 'votaciones',
+    user: 'root',
+    password: '1234',
+    port: '3306'
 });
 
 const getConnection = () => {
