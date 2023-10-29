2a2ba3c396345e37e81f99bb8ffa85c2fc323bf3
Daniel Guzmán <danieljk001@yahoo.com>
2023-10-28 20:44:21 -0500

    cambio

--- src/database/conexion.database.js
+++ src/database/conexion.database.js
@@ -2,7 +2,7 @@ import mysql from "promise-mysql";
 import config from "../config"
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     database: 'votaciones',
     user: 'root',
     password: '1234',
